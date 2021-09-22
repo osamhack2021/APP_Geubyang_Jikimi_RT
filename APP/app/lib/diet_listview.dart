bb9b68f2e733a16607af6eb56b0b9f51d7dffc44
Morivy42 <86469551+Morivy42@users.noreply.github.com>
2021-09-22 07:54:42 +0000

    식단 sample

--- APP/app/lib/diet_listview.dart
+++ APP/app/lib/diet_listview.dart
@@ -14,10 +14,9 @@ class DietListView extends StatelessWidget {
         shrinkWrap: true,
         padding: const EdgeInsets.all(20.0),
         children: const <Widget>[
-        Text("I'm dedicating every day to you"),
-        Text('Domestic life was never quite my style'),
-        Text('When you smile, you knock me out, I fall apart'),
-        Text('And I thought I was so smart'),
+        Text("조식 : 배추김치, 열무김치, 우유, 감귤주스, 찹쌀밥, 얼큰떡만둣국"),
+        Text('중식 : 아욱된장국 새송이쇠고기볶음 미나리무생채 양념통닭'),
+        Text('석식 : 참치마요덮밥, 돼지고기김치찌개, 오징어채무침'),
         ],
       )
     );
