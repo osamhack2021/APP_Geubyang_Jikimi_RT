9297c8f9ff461eb5cdc4673ccac0d3656d48517b
Calvin1225 <90498919+Calvin1225@users.noreply.github.com>
2021-09-27 11:08:23 +0000

    practice

--- playground/practice/vote/lib/main.dart
+++ playground/practice/vote/lib/main.dart
@@ -1,81 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:polls/polls.dart';
 
-class PollsDemo extends StatefulWidget {
-  @override
-  _PollsDemoState createState() => _PollsDemoState();
+void main() {
+  runApp(MyApp());
 }
 
-class _PollsDemoState extends State<PollsDemo> {
-
-  double option1 = 2.0;
-  double option2 = 1.0;
-  double option3 = 4.0;
-  double option4 = 3.0;
-
-  String user = "user@gmail.com";
-  Map usersWhoVoted = {'test@gmail.com': 1, 'deny@gmail.com' : 3, 'kent@gmail.com' : 2,
-    'xyz@gmail.com' : 3};
-  String creator = "admin@gmail.com";
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Flutter Polls Widget Demo"),
-        automaticallyImplyLeading: false,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Polls(
-              children: [
-                Polls.options(title: 'Java', value: option1),
-                Polls.options(title: 'Kotlin', value: option2),
-                Polls.options(title: 'Flutter', value: option3),
-                Polls.options(title: 'React Native', value: option4),
-              ],
-              question: Text('Which Andriod App Development technology used?',
-                style: TextStyle(fontSize: 17),
+    const title = 'Grid List';
+
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(title),
+        ),
+        body: GridView.count(
+          // Create a grid with 2 columns. If you change the scrollDirection to
+          // horizontal, this produces 2 rows.
+          crossAxisCount: 2,
+          // Generate 100 widgets that display their index in the List.
+          children: List.generate(100, (index) {
+            return Center(
+              child: Text(
+                'Item $index',
+                style: Theme.of(context).textTheme.headline5,
               ),
-              currentUser: this.user,
-              creatorID: this.creator,
-              voteData: usersWhoVoted,
-              userChoice: usersWhoVoted[this.user],
-              onVoteBackgroundColor: Colors.cyan,
-              leadingBackgroundColor: Colors.cyan,
-              backgroundColor: Colors.white,
-              onVote: (choice) {
-                print(choice);
-                setState(() {
-                  this.usersWhoVoted[this.user] = choice;
-                });
-                if (choice == 1) {
-                  setState(() {
-                    option1 += 1.0;
-                  });
-                }
-                if (choice == 2) {
-                  setState(() {
-                    option2 += 1.0;
-                  });
-                }
-                if (choice == 3) {
-                  setState(() {
-                    option3 += 1.0;
-                  });
-                }
-                if (choice == 4) {
-                  setState(() {
-                    option4 += 1.0;
-                  });
-                }
-              },
-            ),
-          ],
+            );
+          }),
         ),
       ),
     );
