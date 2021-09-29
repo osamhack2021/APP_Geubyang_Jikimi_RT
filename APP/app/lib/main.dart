42260edc62c3fc1975d5524bb715f3eef99dbfca
Morivy42 <86469551+Morivy42@users.noreply.github.com>
2021-09-29 02:56:45 +0000

    0929

--- APP/app/lib/main.dart
+++ APP/app/lib/main.dart
@@ -15,21 +15,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: '급양지키미',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.deepPurple,
       ),
       home: const MyHomePage(title: '급양지키미'),
@@ -39,16 +29,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -64,116 +44,190 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            const UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage('assets/프로젝트로고.png'),
+                backgroundColor: Colors.white,
+              ),
+              accountName: Text('Morivi'),
+              accountEmail: Text('morivi@morivi.com'),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.person_sharp,
+                color: Colors.grey[850],
+              ),
+              title: const Text('마이페이지'),
+              onTap: () {
+                print('마이페이지 is clicked');
+              },
+              trailing: Icon(
+                Icons.arrow_forward_ios_rounded,
+                color: Colors.grey[850],
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.rule_outlined,
+                color: Colors.grey[850],
+              ),
+              title: const Text('창고정리'),
+              onTap: () {
+                print('창고정리 is clicked');
+              },
+              trailing: Icon(
+                Icons.arrow_forward_ios_rounded,
+                color: Colors.grey[850],
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.qr_code_scanner_rounded,
+                color: Colors.grey[850],
+              ),
+              title: const Text('QR코드 스캔'),
+              onTap: () {
+                print('QR코드 스캔 is clicked');
+              },
+              trailing: Icon(
+                Icons.arrow_forward_ios_rounded,
+                color: Colors.grey[850],
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.logout_rounded,
+                color: Colors.grey[850],
+              ),
+              title: const Text('로그아웃'),
+              onTap: () {
+                print('로그아웃 is clicked');
+              },
+              trailing: Icon(
+                Icons.arrow_forward_ios_rounded,
+                color: Colors.grey[850],
+              ),
+            ),
+          ],
+        ),
+      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            TextButton(onPressed: () {
-                setState(() {
-                  launchBrowser("http://poisonmap.mfds.go.kr/");
-                });
-              },
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const <Widget>[Icon(Icons.link), Text("식중독 지수 예보!")],
-              )),
+            TextButton(
+                onPressed: () {
+                  setState(() {
+                    launchBrowser("http://poisonmap.mfds.go.kr/");
+                  });
+                },
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const <Widget>[
+                    Icon(Icons.link),
+                    Text("식중독 지수 예보!")
+                  ],
+                )),
             const Text('오늘의 식단'),
-            Container(
-              child: const Placeholder(),
-              color: Colors.amber[600],
-              width: 450,
-              height: 200.0,
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Container(
+                child: const Placeholder(),
+                color: Colors.amber[600],
+              ),
             ),
             const SizedBox(
               height: 50,
             ),
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              ConstrainedBox(
-                constraints:
-                    const BoxConstraints.expand(height: 100, width: 100),
-                child: ElevatedButton(
-                  child: const Text('table_cal'),
-                  onPressed: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (_) => const TableEventsExample()),
+            Flexible(
+              child: GridView.count(
+                crossAxisCount: 2,
+                mainAxisSpacing: 35.0,
+                crossAxisSpacing: 35.0,
+                padding: const EdgeInsets.fromLTRB(20.0, 40.0, 40.0, 20.0),
+                childAspectRatio: 1 / 1,
+                children: <Widget>[
+                  TextButton(
+                    style: TextButton.styleFrom(
+                        backgroundColor: Colors.blueAccent,
+                        primary: Colors.black),
+                    child: const Text('표준식단', style: TextStyle(fontSize: 50)),
+                    onPressed: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (_) => const TableEventsExample()),
+                    ),
                   ),
-                ),
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-              ConstrainedBox(
-                constraints:
-                    const BoxConstraints.expand(height: 100, width: 100),
-                child: ElevatedButton(
-                  child: const Text('Basics'),
-                  onPressed: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (_) => const TableBasicsExample()),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                        backgroundColor: Colors.blueAccent,
+                        primary: Colors.black),
+                    child:
+                        const Text('급식 만족도조사', style: TextStyle(fontSize: 50)),
+                    onPressed: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (_) => const HttpAlbum()),
+                    ),
                   ),
-                ),
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-              ConstrainedBox(
-                constraints:
-                    const BoxConstraints.expand(height: 100, width: 100),
-                child: ElevatedButton(
-                  child: const Text('ListView'),
-                  onPressed: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (_) => const DietListView()),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                        backgroundColor: Colors.blueAccent,
+                        primary: Colors.black),
+                    child: const Text('응원의 말', style: TextStyle(fontSize: 50)),
+                    onPressed: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (_) => const HttpAlbum()),
+                    ),
                   ),
-                ),
-              ),
-              const SizedBox(height: 60),
-              ConstrainedBox(
-                constraints:
-                    const BoxConstraints.expand(height: 100, width: 100),
-                child: ElevatedButton(
-                  child: const Text('ListView'),
-                  onPressed: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (_) => const HttpAlbum()),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                        backgroundColor: Colors.blueAccent,
+                        primary: Colors.black),
+                    child: const Text('레시피', style: TextStyle(fontSize: 50)),
+                    onPressed: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (_) => const HttpAlbum()),
+                    ),
                   ),
-                ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                        backgroundColor: Colors.blueAccent,
+                        primary: Colors.black),
+                    child: const Text('브런치데이 의견 종합',
+                        style: TextStyle(fontSize: 50)),
+                    onPressed: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (_) => const HttpAlbum()),
+                    ),
+                  )
+                ],
               ),
-            ]),
+            ),
           ],
         ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  // TextButton mainPageTextButton(BuildContext context, String buttonName, ) {
+  //   return TextButton(
+  //                 style: TextButton.styleFrom(
+  //                     backgroundColor: Colors.blueAccent,
+  //                     primary: Colors.black),
+  //                 child: const Text('레시피', style: TextStyle(fontSize: 50)),
+  //                 onPressed: () => Navigator.push(
+  //                   context,
+  //                   MaterialPageRoute(builder: (_) => const HttpAlbum()),
+  //                 ),
+  //               );
+  // }
 }
