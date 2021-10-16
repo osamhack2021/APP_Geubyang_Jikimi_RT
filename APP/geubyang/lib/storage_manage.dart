6d0dec5c65a8b316ac67d5c6ef07af11106d8c6d
Morivy42 <86469551+Morivy42@users.noreply.github.com>
2021-10-16 10:11:18 +0000

    ----

--- APP/geubyang/lib/storage_manage.dart
+++ APP/geubyang/lib/storage_manage.dart
@@ -1,7 +1,38 @@
 import 'package:flutter/material.dart';
 
-class StorageManage extends StatelessWidget {
+class StorageManage extends StatefulWidget {
   const StorageManage({Key? key}) : super(key: key);
+
+  @override
+  State<StorageManage> createState() => _StorageManageState();
+}
+
+class _StorageManageState extends State<StorageManage> {
+  List<Item> items = [];
+
+  @override
+  void initState() {
+    items = [];
+    super.initState();
+  }
+
+  Widget listViewItem() {
+    return Card(
+      child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+        const ListTile(
+          leading: Icon(Icons.album),
+          title: Text('포카리스웨트'),
+          subtitle: Text('개수: 200'),
+        ),
+        Row(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
+          TextButton(
+            child: const Text('불출'),
+            onPressed: () {/* ... */},
+          ),
+        ]),
+      ]),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,40 +40,148 @@ class StorageManage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('창고'),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Card(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  const ListTile(
-                    leading: Icon(Icons.album),
-                    title: Text('포카리스웨트'),
-                    subtitle:
-                        Text('개수'),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      TextButton(
-                        child: const Text('불출'),
-                        onPressed: () {/* ... */},
-                      ),
-                      // const SizedBox(width: 8),
-                      // TextButton(
-                      //   child: const Text('LISTEN'),
-                      //   onPressed: () {/* ... */},
-                      // ),
-                      // const SizedBox(width: 8),
-                    ],
-                  ),
-                ],
-              ),
-            )
-          ],
-        ),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, i) {
+          return listViewItem();
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            showDatePicker(
+                context: context,
+                firstDate: DateTime.utc(2000, 01, 01),
+                initialDate: DateTime.now(),
+                lastDate: DateTime.utc(2030, 12, 31));
+            _addItem();
+          });
+        },
+        child: const Icon(Icons.add),
+        backgroundColor: Colors.green,
       ),
     );
+  }
+
+  void _addItem() {
+    items.add((Item(
+        name: 'name',
+        productionDate: 'productionDate',
+        ExpirationDate: 'ExpirationDate',
+        quantity: 42)));
+  }
+}
+
+Future<DateTime?> showDatePicker({
+  required BuildContext context,
+  required DateTime initialDate,
+  required DateTime firstDate,
+  required DateTime lastDate,
+  DateTime? currentDate,
+  DatePickerEntryMode initialEntryMode = DatePickerEntryMode.calendar,
+  SelectableDayPredicate? selectableDayPredicate,
+  String? helpText,
+  String? cancelText,
+  String? confirmText,
+  Locale? locale,
+  bool useRootNavigator = true,
+  RouteSettings? routeSettings,
+  TextDirection? textDirection,
+  TransitionBuilder? builder,
+  DatePickerMode initialDatePickerMode = DatePickerMode.day,
+  String? errorFormatText,
+  String? errorInvalidText,
+  String? fieldHintText,
+  String? fieldLabelText,
+}) async {
+  assert(context != null);
+  assert(initialDate != null);
+  assert(firstDate != null);
+  assert(lastDate != null);
+  initialDate = DateUtils.dateOnly(initialDate);
+  firstDate = DateUtils.dateOnly(firstDate);
+  lastDate = DateUtils.dateOnly(lastDate);
+  assert(
+    !lastDate.isBefore(firstDate),
+    'lastDate $lastDate must be on or after firstDate $firstDate.',
+  );
+  assert(
+    !initialDate.isBefore(firstDate),
+    'initialDate $initialDate must be on or after firstDate $firstDate.',
+  );
+  assert(
+    !initialDate.isAfter(lastDate),
+    'initialDate $initialDate must be on or before lastDate $lastDate.',
+  );
+  assert(
+    selectableDayPredicate == null || selectableDayPredicate(initialDate),
+    'Provided initialDate $initialDate must satisfy provided selectableDayPredicate.',
+  );
+  assert(initialEntryMode != null);
+  assert(useRootNavigator != null);
+  assert(initialDatePickerMode != null);
+  assert(debugCheckHasMaterialLocalizations(context));
+
+  Widget dialog = DatePickerDialog(
+    initialDate: initialDate,
+    firstDate: firstDate,
+    lastDate: lastDate,
+    currentDate: currentDate,
+    initialEntryMode: initialEntryMode,
+    selectableDayPredicate: selectableDayPredicate,
+    helpText: helpText,
+    cancelText: cancelText,
+    confirmText: confirmText,
+    initialCalendarMode: initialDatePickerMode,
+    errorFormatText: errorFormatText,
+    errorInvalidText: errorInvalidText,
+    fieldHintText: fieldHintText,
+    fieldLabelText: fieldLabelText,
+  );
+
+  if (textDirection != null) {
+    dialog = Directionality(
+      textDirection: textDirection,
+      child: dialog,
+    );
+  }
+
+  if (locale != null) {
+    dialog = Localizations.override(
+      context: context,
+      locale: locale,
+      child: dialog,
+    );
+  }
+
+  return showDialog<DateTime>(
+    context: context,
+    useRootNavigator: useRootNavigator,
+    routeSettings: routeSettings,
+    builder: (BuildContext context) {
+      return builder == null ? dialog : builder(context, dialog);
+    },
+  );
+}
+
+class Item {
+  final String name;
+  final String productionDate;
+  final String ExpirationDate;
+  final int quantity;
+
+  Item({
+    required this.name,
+    required this.productionDate,
+    required this.ExpirationDate,
+    required this.quantity,
+  });
+
+  Item sampleItem() {
+    return Item(
+        name: 'name',
+        productionDate: 'productionDate',
+        ExpirationDate: 'ExpirationDate',
+        quantity: 42);
   }
 }
